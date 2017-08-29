7d771120a743e84d31958509abccaeb617dc862e
casuldev <andreguerra7@hotmail.com>
2017-08-29 19:50:07 -0300

    first review commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+//scrollify
+$(function() {
+$.scrollify({
+section : "section",
+scrollSpeed: 900,
+touchScroll:true,
+});
+});
+
+  // Page scroll
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
